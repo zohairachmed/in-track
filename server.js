f97ac70de9ddb6116344f8c9192fae8c532ace3d
Zohair Ahmed <zohair.ahmed@charter.com>
2018-05-03 20:20:28 -0600

    More testing for server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname + '/dist/index.html'));
